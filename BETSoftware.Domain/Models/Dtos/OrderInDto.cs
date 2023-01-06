597bbe123afe2196fe7ebe5d42d5cb1cfc53dd0d
mtyide <mtyide@gmail.com>
2023-01-06 11:44:02 +0200

    Removed Date from OrdersInDto though Date can be passed from FE -> Localized date time AND not Server date - team can discuss the workings of dates (persistence)

--- BETSoftware.Domain/Models/Dtos/OrderInDto.cs
+++ BETSoftware.Domain/Models/Dtos/OrderInDto.cs
@@ -2,7 +2,6 @@
 {
     public class OrderInDto
     {
-        public DateTime? Date { get; set; }
         public int? CustomerId { get; set; }
         public bool? ShippingRequired { get; set; }
         public string? ShippingAddress { get; set; }
