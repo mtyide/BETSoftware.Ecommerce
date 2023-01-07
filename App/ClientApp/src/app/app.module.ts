11db0be8283372605d6b635281a0a4bcb5325f39
mtyide <mtyide@gmail.com>
2023-01-08 00:06:37 +0200

    Fixed Router Link bug issue

--- App/ClientApp/src/app/app.module.ts
+++ App/ClientApp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
 import { ErrorComponent } from './error/error.component';
@@ -20,7 +19,6 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     LoginComponent,
     ProductsComponent,
     ErrorComponent
@@ -32,7 +30,6 @@ import { AppRoutingModule } from './app-routing.module';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'login', component: LoginComponent }
     ]),
