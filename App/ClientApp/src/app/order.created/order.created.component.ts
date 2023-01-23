e143336031c4792b368288a02c38ebbbff2ce9ce
mtyide <mtyide@gmail.com>
2023-01-23 20:13:53 +0200

    Added Back to Products link -> Orders Created component

--- App/ClientApp/src/app/order.created/order.created.component.ts
+++ App/ClientApp/src/app/order.created/order.created.component.ts
@@ -9,15 +9,22 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class OrderCreatedComponent {
   constructor(private route: ActivatedRoute, private storage: Storage, private router: Router) { }
 
+  loginId: any;
   id: number = 0;
   token: any;
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => this.id = params['id']);
     this.token = this.storage.getItem('token');
+    this.loginId = this.storage.getItem('id');
+
     if (this.token === null || this.token === undefined
       || this.id === 0 || this.id === undefined) {
       this.router.navigate(['login']);
     }
+  }
+
+  redirectToProducts() {
+    this.router.navigate(['products', this.loginId]);
   }
 }
